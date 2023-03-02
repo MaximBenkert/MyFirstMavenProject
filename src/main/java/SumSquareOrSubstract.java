450fc534ac8c88ad80760c1dc9e3e9804fd66321
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-03-02 10:24:34 +0100

    new maven Project

---
+++ src/main/java/SumSquareOrSubstract.java
@@ -0,0 +1,9 @@
+public class SumSquareOrSubstract {
+    public static int sumSquareOrSubstract(int i, int j) {
+        if (i > j){
+            return i + j;
+        }else{
+            return i * j;
+        }
+    }
+}
---
+++ src/test/java/SumSquareOrSubstractTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SumSquareOrSubstractTest {
+@Test
+    void shouldReturnSum (){
+    //given
+    int i = 7;
+    int j = 5;
+
+    //when
+    int actual = SumSquareOrSubstract.sumSquareOrSubstract(i, j);
+    //then
+    assertEquals(i + j, actual);
+
+    }
+    @Test
+    void shouldReturnSquare (){
+        //given
+        int i = 3;
+        int j = 3;
+        //when
+        int actual = SumSquareOrSubstract.sumSquareOrSubstract(i, j);
+        //then
+        assertEquals(i * j, actual);
+
+    }
+
+}
